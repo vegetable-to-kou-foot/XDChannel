2d97532ae11a72108b72bb5e867c27ab3432b796
vegetable-to-kou-foot <978806694@qq.com>
2020-05-02 16:43:46 +0800

    第3次提交，调通基本任务

---
+++ src/main/java/cn/edu/xidian/dao/AccountDao.java
@@ -0,0 +1,18 @@
+package cn.edu.xidian.dao;
+
+import cn.edu.xidian.domain.Account;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AccountDao {
+
+    @Select("select * from account")
+    List<Account> findAll();
+
+    @Insert("insert into account (name,money) values (#{name},#{money})")
+    void saveAccount(Account account);
+}
